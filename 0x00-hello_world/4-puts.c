819c3cd00d60baa5fb0175f3c05111b0e6d293d3
wathika-eng <wathika02@gmail.com>
2023-07-13 16:32:01 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - program to print value using puts
+* Description - prints the values on the terminal(screen)
+* Return: 0 Sucess
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
