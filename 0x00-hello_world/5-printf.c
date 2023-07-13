b31473f199d8d166a576e319af5724dec13c5a89
wathika-eng <wathika02@gmail.com>
2023-07-13 20:51:38 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - program that prints a value
+* Description: 'with proper grammar, but the outcome is a piece of art,'
+* Return: Always O (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
