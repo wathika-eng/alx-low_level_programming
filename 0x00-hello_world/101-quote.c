c96633dfd9da14c9e70ac8bb5d41087d54d5806e
wathika-eng <wathika02@gmail.com>
2023-07-13 13:11:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print an array
- * description: a function to print an array
+ * main - prints exactly "and that piece of art is useful"
+ * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
