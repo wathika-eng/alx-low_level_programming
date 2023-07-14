afad2e98feddf4d65be0bc8d97ae2448d098319a
wathika-eng <wathika02@gmail.com>
2023-07-14 16:15:14 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  * description - prints all alphabets
+  * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	for (a ; a <= 'z';)
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
