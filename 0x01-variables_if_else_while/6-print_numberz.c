882ef119d5399f8ba3771664d1aa8c66d5c28851
wathika-eng <wathika02@gmail.com>
2023-07-14 23:14:53 +0300

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - start
+  * Description - prints numbers less than 10 uing putchar
+  * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
