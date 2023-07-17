4fa5a60346c2059e6288afc924f34bbb9f0a7a11
wathika-eng <wathika02@gmail.com>
2023-07-17 16:26:06 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main - Start of the program
+  *Description - Prints numbers and letters
+  * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	char b = 'a';
+
+	while (a <= 9)
+	{
+		putchar (a + '0');
+		a++;
+	}
+	while (b < 'g')
+	{
+		putchar (b);
+		b++;
+	}
+
+	return (0);
+}
