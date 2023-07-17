b88b55f7430d25d4bbb69017953aa65dd48b06e5
wathika-eng <wathika02@gmail.com>
2023-07-17 17:43:45 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description - Print 0-9
+  * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i != 10)
+	{
+		putchar(i, +'0');
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+		putchar('\n');
+		return (0);
+}
