8e882b585794b25bc64ba6774bf0ec54bc807a86
wathika-eng <wathika02@gmail.com>
2023-07-18 02:25:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - start of the program
+ * Description - print a combination of numbers
+ * Return : Always 0 (success)
+*/
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+			{
+				putchar('\n');
+			}else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+			return (0);
+}
