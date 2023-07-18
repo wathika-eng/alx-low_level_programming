598805c4c233493322887ae9c30b510bcc1f7d0a
wathika-eng <wathika02@gmail.com>
2023-07-18 21:23:42 +0300

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_last_digit - start
+  * @n: check last digit
+  * Return: lstdgt
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	a = n % 10;
+
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+
+	return (a);
+}
