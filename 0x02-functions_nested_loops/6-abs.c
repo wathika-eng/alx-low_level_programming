cf7a604950fd5f496464bed55d770a3017c85deb
wathika-eng <wathika02@gmail.com>
2023-07-19 21:11:05 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _abs - start
+  *  @n: check absolute value
+  * Return: 1 if larger than 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
