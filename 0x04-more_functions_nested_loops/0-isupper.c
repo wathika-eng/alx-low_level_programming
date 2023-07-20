c71bd68f5248ac47ccb4c3ba1a8337ee0589ff65
wathika-eng <wathika02@gmail.com>
2023-07-20 22:03:12 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isupper - start
+  * @c: check case
+  * Return: 1 if uppercase else 0
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A' && c == 'Z')
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
