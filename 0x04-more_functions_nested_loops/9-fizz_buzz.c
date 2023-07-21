930aa967ceeb1b9a04645c7c6602c53d88c05765
wathika-eng <wathika02@gmail.com>
2023-07-21 16:07:29 +0300

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - start
+  * description: Fizz if multiple of 3, Buzz if of 5
+  * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		} else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		} else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
