04fa3b5ead4ea8d6b30076e919158d30e0f0c2c0
wathika-eng <wathika02@gmail.com>
2023-07-21 23:48:23 +0300

    3-print_remaining_days.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,11 @@
 #include "main.h"
+
 /**
-  * main - Test function for positive or negative
-  * Return: 0
+ * main - Test function for positive or negative
+ * description - n or p 
+ * Return: 0
 */
+
 int main(void)
 {
 	int i;
@@ -11,4 +14,3 @@ int main(void)
 	positive_or_negative(i);
 	return (0);
 }
-
