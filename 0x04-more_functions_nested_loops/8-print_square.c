e2f9e73f424e7670f5065064a995cde7f098cac8
wathika-eng <wathika02@gmail.com>
2023-07-21 14:34:58 +0300

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - start
+ * @size: draw a square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int line, hash, b;
+	char a;
+
+	for (line = 0; line < size; line++)
+	{
+		for (b = 0; b < 1; b++)
+		{
+			a = '#';
+			for (hash = 0; hash < 1; hash++)
+			{
+				_putchar(a);
+			}
+		}
+	}
+	_putchar('\n');
+}
