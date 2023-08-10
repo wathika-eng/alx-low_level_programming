551d2b60aea8599461f2f75487ef3bbd3de24211
wathika-eng <wathika02@gmail.com>
2023-08-10 17:57:41 +0300

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+  *_memset - start
+  * @s: character
+  * @b: char
+  * @n: int
+  * Return: new value for n
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
+	return (s);
+}
