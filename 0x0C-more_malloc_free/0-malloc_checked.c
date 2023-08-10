56f86b1859116fe7c2674006053f17b9166de6c7
wathika-eng <wathika02@gmail.com>
2023-08-10 22:09:13 +0300

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *malloc_checked - start
+  * @b: pointer
+  * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+	if (ptr != NULL)
+	{
+		return (ptr);
+	}
+	exit(98);
+}
