10c44c8e08f1bd14bcabb0b184c3ad9950346b1a
wathika-eng <wathika02@gmail.com>
2023-08-10 23:01:37 +0300

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _realloc - start
+  * @ptr: pointer
+  * @old_size: bytes
+  * @new_size: new mem block
+  * Return: ptr
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *a, *b;
+	int c;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+	a = malloc(new_size);
+	if (!a)
+	{
+		return (NULL);
+	}
+	b = a;
+	if (new_size < old_size)
+	{
+		for (c = 0; c < new_size; c++)
+		{
+			a[c] = b[c];
+		}
+	}
+	if (new_size > old_size)
+	{
+		for (c = 0; c < b; c++)
+		{
+			a[c] = b[a];
+		}
+	}
+	free(a);
+	return (a);
+}
