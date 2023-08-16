52f41f19b0fca4d8ae8c4ea5739ddec7cb7e1fb8
wathika-eng <wathika02@gmail.com>
2023-08-16 22:11:47 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - start
+  * @name: it's name
+  * @age: a float
+  * @owner: who owns
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
