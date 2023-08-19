8f347ee5e19446abd0bbed5962c0c0ab6b0a7457
wathika-eng <wathika02@gmail.com>
2023-08-19 11:14:22 +0300

    4-sum.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - start
+  * description: prints where the program was compiled from
+  * Return: 0 success
+*/
+
+int main(void)
+{
+	char *s;
+	printf("%s\n"__FILE__);
+	return (0);
+}
