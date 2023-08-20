1720ba97b1f8bd3993b6c21de3221f71e6549b43
wathika-eng <wathika02@gmail.com>
2023-08-20 13:09:16 +0300

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - start
+  * @argc: arguments to pass
+  * @argv: params
+  * Return: 0 success
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b, sum;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	sum = 0;
+	if (a < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (b = 0; b < 5 && a >= 0; b++)
+	{
+		while (a >= coins[b])
+		{
+			sum++;
+			a -= coins[a];
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
