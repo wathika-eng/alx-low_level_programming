6c4d2d6e4168e5a51cc50b4ccdfc39e9297b00d7
wathika-eng <wathika02@gmail.com>
2023-08-20 22:13:49 +0300

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a;
 
-	if (size == NULL || cmp == NULL)
+	if (size <= 0 || cmp == NULL)
 	{
 		if (array == NULL)
 		{
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 
 #endif
