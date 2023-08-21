bb26755df4e97eb8daec8a3c85e7aa92ade43ac1
wathika-eng <wathika02@gmail.com>
2023-08-21 11:20:59 +0300

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+/**
+  * main - start
+  * @argc: arguments
+  * @argv: array
+  * Return: 0 success else 1 || 2
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	char *r;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	r = (char *)main;
+	for (b = 0; b < a; b++)
+	{
+		if (b == a - 1)
+		{
+			printf("%02hhx\n", r[b]);
+			break;
+		}
+		printf("%02hhx\n", r[b]);
+	}
+	return (0);
+}
