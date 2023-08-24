5627a4af4c1f314fa2f1d74f6da87f99e3ad7206
wathika-eng <wathika02@gmail.com>
2023-08-24 19:53:28 +0300

    newnode

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,8 +12,11 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		printf("[0] (nil)");
+	while (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		break;
+	}
 	for (count = 0; h != NULL; count++)
 	{
 		printf("[%d] %s\n", h->len, h->str);
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -13,9 +13,12 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
+	unsigned int count;
 	struct list_s *next;
 } list_t;
 
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 
 #endif
