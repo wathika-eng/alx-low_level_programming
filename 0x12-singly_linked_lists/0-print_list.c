fe1b56ac39cf3e55dd591989a89e2f4ee46d4837
wathika-eng <wathika02@gmail.com>
2023-08-24 15:21:40 +0300

    traverse

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * print_list - start
+  * @h: list
+  * Return: elements
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		printf("[0] (nil)");
+	for (count = 0; h != NULL; count++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (count);
+}
