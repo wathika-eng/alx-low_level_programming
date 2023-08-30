863c311f801387720f2fb42981958ee3328d20eb
wathika-eng <wathika02@gmail.com>
2023-08-30 23:17:31 +0300

    End month

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -7,16 +7,16 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *now = *head;
+	/** listint_t *now = *head; */
 	listint_t *mbele = NULL;
 	listint_t *nyuma = NULL;
 
-	for (; now;)
+	for (; *head;)
 	{
-		mbele = now->next;
-		now->next = nyuma;
-		nyuma = now;
-		now = mbele;
+		mbele = (*head)->next;
+		(*head)->next = nyuma;
+		nyuma = *head;
+		*head = mbele;
 	}
 	*head = nyuma;
 	return (*head);
